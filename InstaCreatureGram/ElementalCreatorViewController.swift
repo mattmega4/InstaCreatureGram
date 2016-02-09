a9de2b5f07cd3e072a4bcdfc452ee75d68672772
mattmega4 <mattmega4@gmail.com>
2016-02-08 19:11:11 -0800

    redone

--- InstaCreatureGram/ElementalCreatorViewController.swift
+++ InstaCreatureGram/ElementalCreatorViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ElementalCreatorViewController: UIViewController {
+class ElementalCreatorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var imageView: UIImageView!
     
+    @IBOutlet weak var imageViewOne: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let model: [[UIColor]] = generateRandomData()
+    var storedOffsets = [Int: CGFloat]()
+    
     let filter = CIFilter(name: "CISepiaTone")
+    
     let context = CIContext(options: nil)
     var extent: CGRect!
     var scaleFactor: CGFloat!
@@ -20,16 +27,18 @@ class ElementalCreatorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        
         //IMAGE FILTER FUNCTIONALITY
-        scaleFactor = UIScreen.mainScreen().scale
-        extent = CGRectApplyAffineTransform(UIScreen.mainScreen().bounds, CGAffineTransformMakeScale(scaleFactor, scaleFactor))
-        
-        let ciImage = CIImage(image: imageView.image!)
-        
-        filter?.setDefaults()
-        filter?.setValue(ciImage, forKey: kCIInputImageKey)
-        
-        imageView.image = UIImage(CGImage: context.createCGImage((filter?.outputImage)!, fromRect: extent))
+//        scaleFactor = UIScreen.mainScreen().scale
+//        extent = CGRectApplyAffineTransform(UIScreen.mainScreen().bounds, CGAffineTransformMakeScale(scaleFactor, scaleFactor))
+//        
+////        let ciImage = CIImage(image: imageView.image!)
+//        
+//        filter?.setDefaults()
+////        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+//        
+//        imageView.image = UIImage(CGImage: context.createCGImage((filter?.outputImage)!, fromRect: extent))
     }
     
     //SCREENSHOT FUNCTIONALITY
@@ -46,4 +55,43 @@ class ElementalCreatorViewController: UIViewController {
         UIGraphicsEndImageContext()
         return image
     }
+    
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return model.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("CellT",
+                forIndexPath: indexPath)
+            
+            return cell
+    }
+    
+    func tableView(tableView: UITableView,
+        willDisplayCell cell: UITableViewCell,
+        forRowAtIndexPath indexPath: NSIndexPath) {
+            
+            guard let tableViewCell = cell as? TableViewCell else { return }
+            
+            tableViewCell.setCollectionViewDataSourceDelegate(self, forRow: indexPath.row)
+            tableViewCell.collectionViewOffset = storedOffsets[indexPath.row] ?? 0
+    }
+    
+    func tableView(tableView: UITableView,
+        didEndDisplayingCell cell: UITableViewCell,
+        forRowAtIndexPath indexPath: NSIndexPath) {
+            
+            guard let tableViewCell = cell as? TableViewCell else { return }
+            
+            storedOffsets[indexPath.row] = tableViewCell.collectionViewOffset
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        //this is the delegate method needed
+    }
+    
 }
